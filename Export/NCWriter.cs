4b0fb2233d63fd1826c8de615b275d66e9f1d611
s0566827 <dmitry.savkin@student.htw-berlin.de>
2019-05-10 22:56:21 +0300

    Richtige Dll Bibliotheken wurden erstellt, da die ehemalige Projekte nicht ausföhrbar waren

--- Export/NCWriter.cs
+++ Export/NCWriter.cs
@@ -1,5 +1,4 @@
-﻿using GCodeGenerator;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,13 +6,9 @@ using System.Threading.Tasks;
 
 namespace Export
 {
-   public class NCWriter
+    public class NCWriter
     {
 
-        public static bool Export(GCode gcode, string path)
-        {
-
-            return true;
-        }
     }
 }
+
