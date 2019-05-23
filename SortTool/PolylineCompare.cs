ef5b7405eb3ca6d4dda82145c09d7219ffcd87a5
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2019-05-23 22:05:57 +0200

    manuelkanten implementiert v.1.0

---
+++ SortTool/PolylineCompare.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortTool
+{
+    class PolylineCompare : IComparer<Polyline>
+    {
+        public int Compare(Polyline p1, Polyline p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
